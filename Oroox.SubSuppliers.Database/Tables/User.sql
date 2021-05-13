e556fe60d71f82473915d281a3cd99fd4a0602ac
PawelPotepa <pawel.potepa@oroox.com>
2021-05-13 15:57:33 +0200

    Changer User

--- Oroox.SubSuppliers.Database/Tables/User.sql
+++ Oroox.SubSuppliers.Database/Tables/User.sql
@@ -7,6 +7,5 @@
     [EmailAddress] NVARCHAR(75) NULL, 
     [CompanySizeId] UNIQUEIDENTIFIER NULL, 
     [WebsiteUrl] NCHAR(75) NULL, 
-    [WebsiteUrl] NCHAR(75) NULL, 
     CONSTRAINT [FK_User_Address] FOREIGN KEY ([AddressId]) REFERENCES [Address](Id)
 )
