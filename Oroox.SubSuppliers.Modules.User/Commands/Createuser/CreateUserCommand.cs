29892907e423f6241000756c7f1c2a6224139867
PawelPotepa <pawel.potepa@oroox.com>
2021-05-13 13:48:13 +0200

    Add More Files

---
+++ Oroox.SubSuppliers.Modules.User/Commands/Createuser/CreateUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Oroox.SubSuppliers.Modules.User.Commands
+{
+    public class CreateUserCommand : IRequest<CreateUserCommandResponse>
+    {
+        public string Name { get; set; }
+    }
+}
