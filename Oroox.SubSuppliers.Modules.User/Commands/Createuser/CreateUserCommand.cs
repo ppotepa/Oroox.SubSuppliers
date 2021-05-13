c06512f6b37e6b8e3844c9d0833f33172ac8add0
PawelPotepa <pawel.potepa@oroox.com>
2021-05-13 13:59:17 +0200

    Update Project Tree

--- Oroox.SubSuppliers.Modules.User/Commands/Createuser/CreateUserCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Oroox.SubSuppliers.Modules.User.Commands
-{
-    public class CreateUserCommand : IRequest<CreateUserCommandResponse>
-    {
-        public string Name { get; set; }
-    }
-}
