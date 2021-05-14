17791f054e9320158dc31d3e7341b63c46fb46bd
PawelPotepa <pawel.potepa@oroox.com>
2021-05-14 16:28:41 +0200

    Reorganised bits of code.

--- Oroox.SubSuppliers.Domain/Entity.cs
+++ Oroox.SubSuppliers.Domain/Entity.cs
@@ -3,11 +3,12 @@
 namespace Oroox.SubSuppliers.Domain
 {
     public class Entity 
-    { 
-        public Guid Id { get; set; }
+    {
+        public Guid CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-        public DateTime CreatedBy { get; set; }
-        public DateTime ModifiedOn { get; set; }
         public bool Deleted { get; set; }
+        public Guid Id { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
     }
 }
