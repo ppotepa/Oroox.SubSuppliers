0a46d6a602732320151e3201f5041e3c6caf4fff
PawelPotepa <pawel.potepa@oroox.com>
2021-06-22 16:46:43 +0200

    Add extension

---
+++ Oroox.SubSuppliers.Domain/DomainExtensions/DbContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oroox.SubSuppliers.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Oroox.SubSuppliers.Domain.Extensions
+{
+    public static partial class DbContextExtensions
+    {
+        //public async static Task<TEntity> FindRegardingObject<TEntity>(this DbContext @this, Guid id) where TEntity : Entity
+        //{
+        //    RegardingObject regardingObject = await @this.Set<RegardingObject>()
+        //                        .FirstAsync(obj => obj.EntityName == typeof(TEntity).Name && obj.RegardingObjectId == id);
+        //}
+    }
+}
